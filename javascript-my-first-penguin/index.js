583eaefd4c2d4a2433e563796e613cbdd7698d13
Magnus L. Holtet <magnulho@gmail.com>
2018-11-06 18:37:19 +0100

    Add console.log

--- javascript-my-first-penguin/index.js
+++ javascript-my-first-penguin/index.js
@@ -5,14 +5,17 @@ const RETREAT = "retreat";
 const SHOOT = "shoot";
 const PASS = "pass";
 
-const MOVE_UP =  {"top" : ADVANCE, "bottom" : ROTATE_LEFT, "right" : ROTATE_LEFT ,"left" : ROTATE_RIGHT };
-const MOVE_DOWN =  {"top" : ROTATE_LEFT, "bottom" : ADVANCE, "right" : ROTATE_RIGHT ,"left" : ROTATE_LEFT };
-const MOVE_RIGHT = {"top" : ROTATE_RIGHT, "bottom" : ROTATE_LEFT, "right" : ADVANCE ,"left" : ROTATE_LEFT };
-const MOVE_LEFT = {"top" : ROTATE_LEFT, "bottom" : ROTATE_RIGHT, "right" : ROTATE_RIGHT,"left" : ADVANCE };
+// * 
+const MOVE_UP = { "top": ADVANCE, "bottom": ROTATE_LEFT, "right": ROTATE_LEFT, "left": ROTATE_RIGHT };
+const MOVE_DOWN = { "top": ROTATE_LEFT, "bottom": ADVANCE, "right": ROTATE_RIGHT, "left": ROTATE_LEFT };
+const MOVE_RIGHT = { "top": ROTATE_RIGHT, "bottom": ROTATE_LEFT, "right": ADVANCE, "left": ROTATE_LEFT };
+const MOVE_LEFT = { "top": ROTATE_LEFT, "bottom": ROTATE_RIGHT, "right": ROTATE_RIGHT, "left": ADVANCE };
 
+
+// * Calculates the center of the map
 function moveTowardsCenterOfMap(body) {
-    let centerPointX = Math.floor((body.mapWidth)/2);
-    let centerPointY = Math.floor((body.mapHeight)/2);
+    let centerPointX = Math.floor((body.mapWidth) / 2);
+    let centerPointY = Math.floor((body.mapHeight) / 2);
     return moveTowardsPoint(body, centerPointX, centerPointY);
 }
 
@@ -20,9 +23,9 @@ function moveTowardsPoint(body, pointX, pointY) {
     let penguinPositionX = body.you.x;
     let penguinPositionY = body.you.y;
     let plannedAction = PASS;
-    
+
     if (penguinPositionX < pointX) {
-        plannedAction =  MOVE_RIGHT[body.you.direction];
+        plannedAction = MOVE_RIGHT[body.you.direction];
     } else if (penguinPositionX > pointX) {
         plannedAction = MOVE_LEFT[body.you.direction];
     } else if (penguinPositionY < pointY) {
@@ -36,6 +39,7 @@ function moveTowardsPoint(body, pointX, pointY) {
     return plannedAction
 }
 
+// * Checks if a specific cell contains a wall
 function doesCellContainWall(walls, x, y) {
     if (walls.find(wall => wall.x == x && wall.y == y)) {
         return true;
@@ -43,8 +47,9 @@ function doesCellContainWall(walls, x, y) {
     return false;
 }
 
+// * Checks if it is a wall in front of the penguin
 function wallInFrontOfPenguin(body) {
-    switch(body.you.direction) {
+    switch (body.you.direction) {
         case "top":
             return doesCellContainWall(body.walls, body.you.x, --body.you.y);
         case "bottom":
@@ -61,20 +66,24 @@ function wallInFrontOfPenguin(body) {
 function commandReceived(body) {
     let response = PASS;
     response = moveTowardsCenterOfMap(body);
-    return { command: response};
+    return { 
+        command: response 
+    };
 }
 
+// ! Kalles av Azure
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    let response = action(req);    
+    let response = action(req);
     context.res = {
-        headers: {"Content-Type": 'application/json'},
+        headers: { "Content-Type": 'application/json' },
         body: response
     };
     context.done();
 };
 
 function action(req) {
+    console.log(req);
     if (req.params.query == "command") {
         return commandReceived(req.body);
     } else if (req.params.query == "info") {
@@ -83,9 +92,10 @@ function action(req) {
     return {};
 }
 
+// Returns penguin name and team name
 function infoReceived() {
-    let penguinName = "Pingu";
-    let teamName = "Bouvet";
+    let penguinName = "Randyyyy";
+    let teamName = "Markus and the bois";
 
-    return {name: penguinName, team: teamName};
+    return { name: penguinName, team: teamName };
 }
