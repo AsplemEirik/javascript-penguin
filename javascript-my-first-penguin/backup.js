519a4403c66f81a72488cda4ee6f630898842147
Magnus L. Holtet <magnulho@gmail.com>
2018-11-06 18:55:55 +0100

    Split code into modules.

--- javascript-my-first-penguin/backup.js
+++ javascript-my-first-penguin/backup.js
@@ -1,5 +1,3 @@
-import * as Logic from 'logic.js';
-
 const ROTATE_LEFT = "rotate-left";
 const ROTATE_RIGHT = "rotate-right";
 const ADVANCE = "advance";
@@ -8,16 +6,16 @@ const SHOOT = "shoot";
 const PASS = "pass";
 
 // * 
-const MOVE_UP = { "top": ADVANCE, "bottom": ROTATE_LEFT, "right": ROTATE_LEFT, "left": ROTATE_RIGHT };
-const MOVE_DOWN = { "top": ROTATE_LEFT, "bottom": ADVANCE, "right": ROTATE_RIGHT, "left": ROTATE_LEFT };
-const MOVE_RIGHT = { "top": ROTATE_RIGHT, "bottom": ROTATE_LEFT, "right": ADVANCE, "left": ROTATE_LEFT };
-const MOVE_LEFT = { "top": ROTATE_LEFT, "bottom": ROTATE_RIGHT, "right": ROTATE_RIGHT, "left": ADVANCE };
+const MOVE_UP =  {"top" : ADVANCE, "bottom" : ROTATE_LEFT, "right" : ROTATE_LEFT ,"left" : ROTATE_RIGHT };
+const MOVE_DOWN =  {"top" : ROTATE_LEFT, "bottom" : ADVANCE, "right" : ROTATE_RIGHT ,"left" : ROTATE_LEFT };
+const MOVE_RIGHT = {"top" : ROTATE_RIGHT, "bottom" : ROTATE_LEFT, "right" : ADVANCE ,"left" : ROTATE_LEFT };
+const MOVE_LEFT = {"top" : ROTATE_LEFT, "bottom" : ROTATE_RIGHT, "right" : ROTATE_RIGHT,"left" : ADVANCE };
 
 
 // * Calculates the center of the map
 function moveTowardsCenterOfMap(body) {
-    let centerPointX = Math.floor((body.mapWidth) / 2);
-    let centerPointY = Math.floor((body.mapHeight) / 2);
+    let centerPointX = Math.floor((body.mapWidth)/2);
+    let centerPointY = Math.floor((body.mapHeight)/2);
     return moveTowardsPoint(body, centerPointX, centerPointY);
 }
 
@@ -25,9 +23,9 @@ function moveTowardsPoint(body, pointX, pointY) {
     let penguinPositionX = body.you.x;
     let penguinPositionY = body.you.y;
     let plannedAction = PASS;
-
+    
     if (penguinPositionX < pointX) {
-        plannedAction = MOVE_RIGHT[body.you.direction];
+        plannedAction =  MOVE_RIGHT[body.you.direction];
     } else if (penguinPositionX > pointX) {
         plannedAction = MOVE_LEFT[body.you.direction];
     } else if (penguinPositionY < pointY) {
@@ -51,7 +49,7 @@ function doesCellContainWall(walls, x, y) {
 
 // * Checks if it is a wall in front of the penguin
 function wallInFrontOfPenguin(body) {
-    switch (body.you.direction) {
+    switch(body.you.direction) {
         case "top":
             return doesCellContainWall(body.walls, body.you.x, --body.you.y);
         case "bottom":
@@ -67,26 +65,21 @@ function wallInFrontOfPenguin(body) {
 
 function commandReceived(body) {
     let response = PASS;
-    response = calculateMove(body);
-
-    return { 
-        command: response 
-    };
+    response = moveTowardsCenterOfMap(body);
+    return { command: response};
 }
 
-// ! Kalles av Azure
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    let response = action(req);
+    let response = action(req);    
     context.res = {
-        headers: { "Content-Type": 'application/json' },
+        headers: {"Content-Type": 'application/json'},
         body: response
     };
     context.done();
 };
 
 function action(req) {
-    context.log(req);
     if (req.params.query == "command") {
         return commandReceived(req.body);
     } else if (req.params.query == "info") {
@@ -100,7 +93,7 @@ function infoReceived() {
     let penguinName = "Randyyyy";
     let teamName = "Markus and the bois";
 
-    return { name: penguinName, team: teamName };
+    return {name: penguinName, team: teamName};
 }
 
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
